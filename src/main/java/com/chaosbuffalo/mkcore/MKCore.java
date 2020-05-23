84406982fa22b1ba1551dc518e21d7921bfd469c
Lee Harrison <ralekdev@gmail.com>
2020-05-22 22:25:22 -0700

    add ability registry

--- src/main/java/com/chaosbuffalo/mkcore/MKCore.java
+++ src/main/java/com/chaosbuffalo/mkcore/MKCore.java
@@ -65,17 +65,6 @@ public class MKCore {
         MKCommand.register(event.getCommandDispatcher());
     }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
-
     public static ResourceLocation makeRL(String path) {
         return new ResourceLocation(MKCore.MOD_ID, path);
     }
--- src/main/java/com/chaosbuffalo/mkcore/MKCoreRegistry.java
+++ src/main/java/com/chaosbuffalo/mkcore/MKCoreRegistry.java
@@ -1,8 +1,32 @@
 package com.chaosbuffalo.mkcore;
 
+import com.chaosbuffalo.mkcore.abilities.PlayerAbility;
+import net.minecraft.block.Block;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.RegistryBuilder;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MKCoreRegistry {
     public static ResourceLocation INVALID_CLASS = new ResourceLocation(MKCore.MOD_ID, "class.invalid");
     public static ResourceLocation INVALID_ABILITY = new ResourceLocation(MKCore.MOD_ID, "ability.invalid");
+    public static IForgeRegistry<PlayerAbility> ABILITIES = null;
+
+    @SubscribeEvent
+    public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+        // register a new block here
+        MKCore.LOGGER.info("HELLO from Register Block");
+    }
+
+    @SubscribeEvent
+    public static void createRegistries(RegistryEvent.NewRegistry event) {
+        ABILITIES = new RegistryBuilder<PlayerAbility>()
+                .setName(MKCore.makeRL("abilities"))
+                .setType(PlayerAbility.class)
+                .setIDRange(0, Integer.MAX_VALUE - 1)
+                .create();
+    }
 }
