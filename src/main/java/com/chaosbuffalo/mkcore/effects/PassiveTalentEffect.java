e50f509189bf85b3b3fdf1c0d1e19dcd251572fa
Lee Harrison <ralekdev@gmail.com>
2020-06-14 22:31:02 -0700

    Fix expiration of non-infinite passives

--- src/main/java/com/chaosbuffalo/mkcore/effects/PassiveTalentEffect.java
+++ src/main/java/com/chaosbuffalo/mkcore/effects/PassiveTalentEffect.java
@@ -6,4 +6,9 @@ public abstract class PassiveTalentEffect extends PassiveEffect {
     protected PassiveTalentEffect() {
         super(EffectType.BENEFICIAL, 0);
     }
+
+    @Override
+    public boolean isInfiniteDuration() {
+        return true;
+    }
 }
