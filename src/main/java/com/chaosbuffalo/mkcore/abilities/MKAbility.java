b3e4c445709af8619f0266659e36e83cfaa57f5d
Lee Harrison <ralekdev@gmail.com>
2020-06-28 00:07:52 -0700

    Support toggle abilities with cast times

--- src/main/java/com/chaosbuffalo/mkcore/abilities/MKAbility.java
+++ src/main/java/com/chaosbuffalo/mkcore/abilities/MKAbility.java
@@ -249,7 +249,7 @@ public abstract class MKAbility extends ForgeRegistryEntry<MKAbility> {
     }
 
     public void executeWithContext(IMKEntityData entityData, AbilityContext context) {
-        entityData.startAbility(context, this);
+        entityData.getAbilityExecutor().startAbility(context, this);
     }
 
     public AbilityTargetSelector getTargetSelector() {
